0d1f2f813770812c0ac9a8a0336ab566a11c4099
Taigo Ito <webmaster@qwel.design>
2024-12-03 19:26:22 +0900

    Insert a drawer menu

---
+++ js/drawerMenu.js
@@ -0,0 +1,210 @@
+/**
+ * Drawer Menu
+ * Author: Taigo Ito (https://qwel.design/)
+ * Location: Fukui, Japan
+ */
+
+class DrawerMenu {
+  
+  constructor(options = {}) {
+    // 設定
+    this.darkMode = options.darkMode || false;
+
+    // クローンする対象
+    this._siteBrand = options.siteBrand ||
+      document.querySelector('.header__siteBrand');
+    this._primaryMenu = options.primaryMenu ||
+      document.querySelector('.header__primaryMenu');
+    this._tel = options.tel ||
+      document.querySelector('.header__tel');
+
+    // 各要素生成
+    // .drawer
+    this._drawer = document.createElement('button');
+    this._drawer.classList.add('drawer');
+    if (this.darkMode) this._drawer.classList.add('--dark');
+
+    // .drawer__navicon
+    this._navicon = document.createElement('span');
+    this._navicon.classList.add('drawer__navicon');
+    this._navicon.dataset.icon = 'ei-navicon';
+    this._navicon.dataset.size = 'm';
+    this._drawer.appendChild(this._navicon);
+
+    // .drawer__close
+    this._close = document.createElement('span');
+    this._close.classList.add('drawer__close');
+    this._close.dataset.icon = 'ei-close';
+    this._close.dataset.size = 'm';
+    this._drawer.appendChild(this._close);
+
+    // .drawerMenu
+    this._drawerMenu = document.createElement('div');
+    this._drawerMenu.classList.add('drawerMenu');
+    if (this.darkMode) this._drawerMenu.classList.add('--dark');
+
+    // .drawerMenu__inner
+    this._menu = document.createElement('div');
+    this._menu.classList.add('drawerMenu__inner');
+    this._drawerMenu.appendChild(this._menu);
+    this._importMenu();
+
+    // .drawerMenuOverlay
+    this._overlay = document.createElement('div');
+    this._overlay.classList.add('drawerMenuOverlay', '--collapse');
+
+    // body要素に挿入
+    const body = document.body;
+    body.appendChild(this._drawer);
+    body.appendChild(this._drawerMenu);
+    body.appendChild(this._overlay);
+
+    // 状態
+    this.isShown = false;
+
+    // イベント登録
+    this._handleEvents();
+
+  }
+
+
+  toggle() {
+    // 状態から判別して、表示/非表示を切り替え
+    if (this.isShown) this.hide();
+    else this.show();
+
+  }
+
+
+  show() {
+    // 表示
+    if (!this.isShown) {
+      this._transitionEnd(this._drawerMenu, () => {
+        this._drawerMenu.classList.add('--show');
+        this._drawer.classList.add('--active');
+        this._menu.classList.remove('--collapse');
+        this._overlay.classList.remove('--collapse');
+      }).then(() => {
+        this._menu.classList.add('--show');
+      });
+    }
+    this.isShown = true;
+
+  }
+
+
+  hide() {
+    // 非表示
+    if (this.isShown) {
+      this._transitionEnd(this._drawerMenu, () => {
+        this._drawerMenu.classList.remove('--show');
+        this._drawer.classList.remove('--active');
+        this._menu.classList.remove('--show');
+      }).then(() => {
+        this._menu.classList.add('--collapse');
+        this._overlay.classList.add('--collapse');
+      });
+    }
+    this.isShown = false;
+
+  }
+
+
+  _importMenu() {
+    // メニューアイテムをインポート
+    if (this._siteBrand) {
+      this._importSiteBrand(); // .drawerMenu__siteBrand
+    }
+    if (this._primaryMenu) {
+      this._importPrimaryMenu(); // .drawerMenu__primaryMenu
+    }
+    if (this._tel) {
+      this._importTel(); // .drawerMenu__tel
+    }
+  }
+
+
+  _importSiteBrand() {
+    // ブランドロゴ・タイトルをインポート
+    const siteBrand = document.createElement('div');
+    siteBrand.classList.add('drawerMenu__item', '--siteBrand');
+    siteBrand.appendChild(this._siteBrand.cloneNode(true));
+    this._menu.appendChild(siteBrand);
+
+  }
+
+
+  _importPrimaryMenu() {
+    // プライマリメニューをインポート
+    const primaryMenu = document.createElement('ul');
+    primaryMenu.classList.add('drawerMenu__primaryMenu');
+
+    // li要素を順次インポート
+    const menuItems = this._primaryMenu.querySelectorAll('li');
+    menuItems.forEach((menuItem) => {
+      const primaryMenuItem = document.createElement('li');
+      primaryMenuItem.classList.add('drawerMenu__item');
+      primaryMenuItem.innerHTML = menuItem.innerHTML;
+      primaryMenu.appendChild(primaryMenuItem);
+    });
+    this._menu.appendChild(primaryMenu);
+
+  }
+
+
+  _importTel() {
+    // 電話番号リンク画像をインポート
+    const tel = document.createElement('div');
+    tel.classList.add('drawerMenu__item', '--tel');
+    tel.appendChild(this._tel.cloneNode(true));
+    this._menu.appendChild(tel);
+
+  }
+
+
+  _handleEvents() {
+    //const myTouch = 'ontouchend' in document && window.innerWidth < 1024 ? 'touchend' : 'click';
+    const myTouch = 'click';
+    
+    // ドロワーのイベント登録
+    this._drawer.addEventListener(myTouch, (event) => {
+      event.preventDefault();
+      this.toggle();
+    });
+
+    // オーバーレイのイベント登録
+    this._overlay.addEventListener(myTouch, () => {
+      this.hide();
+    });
+
+    // スクロール時のイベント登録
+    window.addEventListener('scroll', () => {
+      this._windowScrollHandler();
+    });
+
+  }
+
+
+  _windowScrollHandler() {
+    // スクロール時にメニューを非表示
+    if (this.isShown) this.hide();
+
+  }
+
+
+  _transitionEnd(elem, func) {
+    // CSS遷移の完了を監視
+    let callback;
+    const promise = new Promise((resolve, reject) => {
+      callback = () => resolve(elem);
+      elem.addEventListener('transitionend', callback);
+    });
+    func();
+    promise.then((elem) => {
+      elem.removeEventListener('transitionend', callback);
+    });
+    return promise;
+
+  }
+
+}
--- js/init.js
+++ js/init.js
@@ -4,6 +4,14 @@
  * Location: Fukui, Japan
  */
 
+// Drawer Menu
+//import DrawerMenu from './drawerMenu.js';
+new DrawerMenu();
+
+// Evil Icons
+//import EvilIcons from './evilIcons.js';
+new EvilIcons();
+
 // Slider
 //import Slider from './slider.js';
 new Slider();
